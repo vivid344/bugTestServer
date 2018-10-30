a283118ac08de4d3bab59c2649ba39aafe868c87
vivid344 <vivid344@icloud.com>
2018-10-30 16:46:25 +0900

    [fix]jsonを受け取るように変更

--- main.go
+++ main.go
@@ -1,21 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Send_json struct {
+	User     string `form:"name" json:"name"`
+	Password string `form:"raw_password" json:"raw_password"`
+}
 
 func main(){
 	r := gin.Default()
 
 	r.POST("/", func(c *gin.Context) {
-		name := c.PostForm("name")
-		pass := c.PostForm("raw_password")
-
-		c.JSON(200, gin.H{
-			"name": name,
-			"pass": pass,
-		})
+		var json Send_json
+		if c.BindJSON(&json) == nil {
+			c.JSON(200, gin.H{
+				"name": json.User,
+				"pass": json.Password,
+			})
+		}
 	})
-
 	r.Run(":8080")
 }
 
-// curlコマンド　curl -X POST http://localhost:8080 -d "name=hoge&raw_password=huga"
+// curlコマンド　curl -v http://localhost:8080/ -H "Content-Type: application/json" -X POST -d '{"name":"hoge","raw_password":"huga"}'
